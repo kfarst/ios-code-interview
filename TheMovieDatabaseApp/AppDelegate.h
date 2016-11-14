f65228e7c4c9ee31b20ac6f7694816cfb11b50f4
Kevin Farst <kevin.farst@zozi.com>
2016-11-13 20:40:26 -0800

    Initial Commit

---
+++ TheMovieDatabaseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheMovieDatabaseApp
+//
+//  Created by Kevin Farst on 11/13/16.
+//  Copyright © 2016 Kevin Farst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
